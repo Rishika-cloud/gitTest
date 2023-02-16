1ed5aae13a43b8568be67e9b15367049a61ef1b5
Rishika-cloud <71088965+Rishika-cloud@users.noreply.github.com>
2023-02-16 22:43:00 +0530

    added main function

--- Student.java
+++ Student.java
@@ -1,7 +1,11 @@
-class Student
+public class Student
 {
   Student()
   {
     System.out.println("I am Happy");
+  }
+  public static void main(String [] args)
+  {
+    Student s= new Student();
   }
 }
