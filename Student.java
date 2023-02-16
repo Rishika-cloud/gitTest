a8a3a58e6f40652a694b94de5ddd16713768d256
Rishika-cloud <71088965+Rishika-cloud@users.noreply.github.com>
2023-02-16 22:50:03 +0530

    Added display function

--- Student.java
+++ Student.java
@@ -4,6 +4,11 @@ public class Student
   {
     System.out.println("I am Happy");
   }
+  void display()
+  {
+    System.out.println("I'm Student");
+  }
+  
   public static void main(String [] args)
   {
     Student s= new Student();
